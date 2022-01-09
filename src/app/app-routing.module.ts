9fb2771165ddb101b2cce0037c8148bdf366409c
Ranjeet Patel <35516909+ranjeetpatel10@users.noreply.github.com>
2022-01-09 12:21:39 +0530

    lazy load implementation.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },  
+  { path: 'orders', loadChildren: ()=> import ('./orders/orders.module').then(m => m.OrdersModule) },
+  { path: '', component: HomeComponent }, 
+  { path: '**', component: PagenotfoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
